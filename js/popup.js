e55b0c22e2be2f207e37a0cce17e88fb1e41117a
Артур Минасян < >
2016-12-06 14:55:50 +0200

    fdsfsd

---
+++ js/popup.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    // вешаем на каждую ссылку обработчик
+$(".switch").click(function(){
+  // удаляем у старого элемента, класс   activeClass    
+  $("body").removeClass("white");
+
+  // добавляем к ссылки по которой щёлкнули класс activeClass
+  $("body").addClass("grey darken-3");
+ 
+
+});
+
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://weather.i.ua/", true);
+    xhr.send(null);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) 
+        {
+            if (xhr.responseText) 
+            {
+                var data = xhr.responseText;
+
+                var last_video = $('.title', data);
+               
+                $('#title').html(last_video);
+              
+
+            }
+        }
+    }
+});
+  
